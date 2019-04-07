6a0b8e18eea60fd8336d31165855e0bfcdfeed86
Brian <riceb53@gmail.com>
2019-04-07 12:14:30 -0500

    add code to wait 2 seconds then print 'first message'

---
+++ async.js
@@ -0,0 +1,3 @@
+setTimeout(function() {
+  console.log('first message');
+}, 2000);
---
+++ sync.rb
@@ -0,0 +1,5 @@
+# have the file wait for 2 seconds, then print out 'first message'
+
+sleep 2
+
+p 'first message'
